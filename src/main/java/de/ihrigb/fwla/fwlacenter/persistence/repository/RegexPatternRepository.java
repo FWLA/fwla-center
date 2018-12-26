fa3c769eadd2ea4ad637f986ec0d02faf8696130
Benjamin Ihrig <benjamin.ihrig@gmail.com>
2018-12-26 21:30:28 +0100

    Fix #5

--- src/main/java/de/ihrigb/fwla/fwlacenter/persistence/repository/RegexPatternRepository.java
+++ src/main/java/de/ihrigb/fwla/fwlacenter/persistence/repository/RegexPatternRepository.java
@@ -1,8 +1,12 @@
 package de.ihrigb.fwla.fwlacenter.persistence.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import de.ihrigb.fwla.fwlacenter.persistence.model.RegexPattern;
+import de.ihrigb.fwla.fwlacenter.persistence.model.Source;
 
 public interface RegexPatternRepository extends JpaRepository<RegexPattern, String> {
+	Optional<RegexPattern> findBySource(Source source);
 }
---
+++ src/test/java/de/ihrigb/fwla/fwlacenter/mail/parse/RegexTest.java
@@ -0,0 +1,47 @@
+package de.ihrigb.fwla.fwlacenter.mail.parse;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import de.ihrigb.fwla.fwlacenter.persistence.model.RegexPattern;
+import de.ihrigb.fwla.fwlacenter.persistence.repository.RegexPatternRepository;
+import de.ihrigb.fwla.fwlacenter.services.api.Operation;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class RegexTest {
+
+	@Autowired
+	private RegexPatternRepository repository;
+
+	@Test
+	public void testCodePattern() throws Exception {
+		assertEquals("F-2", parse("1234567890 / F-2 - , Musterstadt-Musterort, Musterstraße", Fields.CODE).getCode());
+		assertEquals("H-PWASS", parse("1234567890 / H-PWASS - , Musterstadt-Musterort, Fluss - Ort > bis Musterstadt", Fields.CODE).getCode());
+	}
+
+	private Operation parse(String heystack, Fields field) {
+		RegexPattern regexPattern = getPattern(field);
+		Pattern pattern = Pattern.compile(regexPattern.getPattern());
+		Matcher matcher = pattern.matcher(heystack);
+		Operation operation = new Operation();
+
+		field.getPopulator().accept(operation, matcher);
+
+		return operation;
+	}
+
+	private RegexPattern getPattern(Fields field) {
+		return repository.findById(field.getName()).orElseThrow(() -> {
+			return new AssertionError("Did not find pattern for field.");
+		});
+	}
+}
