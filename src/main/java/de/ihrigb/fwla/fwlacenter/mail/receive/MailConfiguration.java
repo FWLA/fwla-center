05245bc8c0960d31d736d4cafe0570a647402c3c
Benjamin Ihrig <benjamin.ihrig@gmail.com>
2019-02-06 12:48:53 +0100

    Fix condition

--- src/main/java/de/ihrigb/fwla/fwlacenter/mail/receive/MailConfiguration.java
+++ src/main/java/de/ihrigb/fwla/fwlacenter/mail/receive/MailConfiguration.java
@@ -20,7 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Configuration
 @EnableIntegration
-@ConditionalOnProperty("mail.receive.host")
+@ConditionalOnProperty("app.mail.receive.host")
 @EnableConfigurationProperties(ReceivingProperties.class)
 public class MailConfiguration {
 
