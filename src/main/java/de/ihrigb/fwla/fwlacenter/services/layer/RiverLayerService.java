a19d4ab9fa9279ec0fd8d4cb8bd0a5803e96a13e
Benjamin Ihrig <benjamin.ihrig@gmail.com>
2019-02-19 15:17:23 +0100

    Clean code and comments

--- src/main/java/de/ihrigb/fwla/fwlacenter/services/layer/RiverLayerService.java
+++ src/main/java/de/ihrigb/fwla/fwlacenter/services/layer/RiverLayerService.java
@@ -31,6 +31,25 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class RiverLayerService implements LayerService {
 
+	private static Pattern layerIdPattern = Pattern.compile("river-(?<bwastrid>\\d+)");
+
+	private static Optional<String> extractBWaStrId(String layerId) {
+		Matcher matcher = layerIdPattern.matcher(layerId);
+		if (!matcher.matches()) {
+			return Optional.empty();
+		}
+		return Optional.ofNullable(matcher.group("bwastrid"));
+	}
+
+	private static boolean isInFehlkilometer(float km, List<Fehlkilometer> fehlkilometer) {
+		for (Fehlkilometer fk : fehlkilometer) {
+			if (fk.getKmVon() < km && fk.getKmBis() > km) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	private final RiverSectorRepository repository;
 	private final CachingWSVRestServiceClient restClient;
 
@@ -50,33 +69,37 @@ public class RiverLayerService implements LayerService {
 			return Collections.emptySet();
 		}
 
-		Pattern pattern = Pattern.compile("river-(?<bwastrid>\\d+)");
-		Matcher matcher = pattern.matcher(layer);
-		if (!matcher.matches()) {
+		Optional<String> optBWaStrId = RiverLayerService.extractBWaStrId(layer);
+		if (!optBWaStrId.isPresent()) {
 			return Collections.emptySet();
 		}
 
-		String bWaStrId = matcher.group("bwastrid");
+		String bWaStrId = optBWaStrId.get();
 		River river = River.ofBWaStrId(bWaStrId);
 		Set<Float> km = new HashSet<>();
 
+		// (1) get all river sectors of this river and add kms by interval to set.
 		repository.findByRiver(river).forEach(riverSector -> {
 			for (float i = riverSector.getFrom(); i <= riverSector.getTo(); i += riverSector.getInterval()) {
 				km.add(i);
 			}
 		});
 
+		// (2) put set into list and sort
 		List<Float> kmsList = new ArrayList<>(km);
 		Collections.sort(kmsList);
 
+		// (3) get fehlkilometer
 		List<Fehlkilometer> fehlkilometer = restClient
 				.fehlkilometer(bWaStrId, kmsList.get(0), kmsList.get(kmsList.size() - 1)).getResult().get(0)
 				.getFehlkilometer();
 
-		kmsList.removeIf(k -> isInFehlkilometer(k, fehlkilometer));
+		// (4) remove from list, if within fehlkilometer
+		kmsList.removeIf(k -> RiverLayerService.isInFehlkilometer(k, fehlkilometer));
 
 		List<GeocodierungQuery> queries = new ArrayList<>();
 
+		// (5) build queries with index as qid
 		for (int i = 0; i < kmsList.size(); i++) {
 			GeocodierungQuery query = new GeocodierungQuery();
 			query.setQid(i);
@@ -88,8 +111,10 @@ public class RiverLayerService implements LayerService {
 			queries.add(query);
 		}
 
+		// (6) execute queries
 		RootResult<GeocodierungResult> result = restClient.geocode(queries);
 
+		// (7) build features from results
 		return result.getResult().stream().map(res -> {
 			Coordinate coordinate = Coordinate.of(res.getGeometry());
 			return new PointFeature(String.format(Locale.US, "%f", kmsList.get(res.getQid())), coordinate, "blue");
@@ -102,25 +127,11 @@ public class RiverLayerService implements LayerService {
 			return Optional.empty();
 		}
 
-		Pattern pattern = Pattern.compile("river-(?<bwastrid>\\d+)");
-		Matcher matcher = pattern.matcher(layer);
-		if (!matcher.matches()) {
-			return Optional.empty();
-		}
-
-		String bWaStrId = matcher.group("bwastrid");
-		River river = River.ofBWaStrId(bWaStrId);
-
-		return Optional.of(
-				new FeatureDetails(river.getName(), String.format(Locale.GERMANY, "Kilometer %f", Float.parseFloat(featureId)), null));
-	}
-
-	private boolean isInFehlkilometer(float km, List<Fehlkilometer> fehlkilometer) {
-		for (Fehlkilometer fk : fehlkilometer) {
-			if (fk.getKmVon() < km && fk.getKmBis() > km) {
-				return true;
-			}
-		}
-		return false;
+		Optional<String> optBWaStrId = RiverLayerService.extractBWaStrId(layer);
+		return optBWaStrId.map(bWaStrId -> {
+			River river = River.ofBWaStrId(bWaStrId);
+			return new FeatureDetails(river.getName(),
+					String.format(Locale.GERMANY, "Kilometer %f", Float.parseFloat(featureId)), null);
+		});
 	}
 }
