7dd0d31d33446bf9198e9c117f6cf97b5d4e9e64
Benjamin Ihrig <benjamin.ihrig@gmail.com>
2019-11-12 12:38:48 +0100

    Enhance geojson utils

--- src/main/java/de/ihrigb/fwla/fwlacenter/utils/GeoJsonUtils.java
+++ src/main/java/de/ihrigb/fwla/fwlacenter/utils/GeoJsonUtils.java
@@ -1,5 +1,6 @@
 package de.ihrigb.fwla.fwlacenter.utils;
 
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collector;
 
@@ -12,16 +13,36 @@ import de.ihrigb.fwla.fwlacenter.api.Coordinate;
 
 public final class GeoJsonUtils {
 
-	public static Coordinate fromLngLatAlt(LngLatAlt lngLatAlt) {
+	public static Function<Coordinate, LngLatAlt> TO_LNG_LAT_ALT = coordinate -> {
+		return new LngLatAlt(coordinate.getLongitude(), coordinate.getLatitude());
+	};
+
+	public static Function<LngLatAlt, Coordinate> FROM_LNG_LAT_ALT = lngLatAlt -> {
 		return new Coordinate(lngLatAlt.getLatitude(), lngLatAlt.getLongitude());
+	};
+
+	public static Function<Coordinate, Point> TO_POINT = coordinate -> {
+		return new Point(GeoJsonUtils.toLngLatAlt(coordinate));
+	};
+
+	public static Coordinate fromLngLatAlt(LngLatAlt lngLatAlt) {
+		return FROM_LNG_LAT_ALT.apply(lngLatAlt);
+	}
+
+	public static Optional<Coordinate> fromLngLatAlt(Optional<LngLatAlt> lngLatAlt) {
+		return lngLatAlt.map(FROM_LNG_LAT_ALT);
 	}
 
 	public static LngLatAlt toLngLatAlt(Coordinate coordinate) {
-		return new LngLatAlt(coordinate.getLongitude(), coordinate.getLatitude());
+		return TO_LNG_LAT_ALT.apply(coordinate);
+	}
+
+	public static Optional<LngLatAlt> toLngLatAlt(Optional<Coordinate> coordinate) {
+		return coordinate.map(TO_LNG_LAT_ALT);
 	}
 
 	public static Point toPoint(Coordinate coordinate) {
-		return new Point(GeoJsonUtils.toLngLatAlt(coordinate));
+		return TO_POINT.apply(coordinate);
 	}
 
 	public static Collector<Feature, ?, FeatureCollection> collector() {
